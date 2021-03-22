3ac1579f879c02a413dd5ce325a1aaf61d0f0ec8
Dalton Chichester <dchiches@mail.umw.edu>
2021-03-22 12:12:35 -0400

    actual fix for passhasher

---
+++ JS_Scripts/PassHasher/user.routes.js
@@ -0,0 +1,62 @@
+module.exports = (app) => {
+     const User = require('./user.model')
+     const {
+         hash,
+         verify
+     } = require('./index');
+
+     app.post('/register', async (req, res) => 
+     {
+         try {
+             let user = new User(
+             {
+                 name: req.body.name,
+                 email: req.body.email,
+                 password: await hash(req.body.password)
+             })
+
+             let response = await user.save();
+             res.status(200).json(
+             {
+                 status: "Success",
+                 data: response
+             })
+         } catch (err) {
+             //handle error
+         }
+     });
+
+     app.post('/login', async (req, res) => 
+     {
+         try {
+             let {
+                 name,
+                 password
+             } = req.body;
+
+             let user = await User.findOne({
+                 name: name
+             })
+             
+             if (!user) {
+                 return res.status(400).json({
+                     type: "Not Found",
+                     msg: "Wrong Login Details"
+                 })
+             }
+
+             let match = await verify(user.password, password);
+
+             if (match) {
+                 res.status(200).json(
+                 {
+                     status: "Success",
+                     message: "Correct Details",
+                     data: user
+                 })
+             }
+         } catch (err) {
+             // handle error
+         }
+     })
+ };
