f9deb773834d8960efd47107ee6107eb0d1d94b8
Nick Turner <nickpturner98@protonmail.com>
2021-04-28 13:56:48 -0400

    should be working with js files and html_Pages

--- server.js
+++ server.js
@@ -6,13 +6,13 @@ var app = require('http').createServer(createServer);
 
 function createServer(req, res) {
     var path = url.parse(req.url).pathname;
-    var fsCallback = function(error, data) {
-        if(error){
+    var fsCallback = function(error, data) { 
+        if(error){ //catches errors
             res.writeHead(404);
             res.write(data);
             res.end();
             doc = fs.readFile(__dirname + "/404.html", fsCallback);
-            throw error;
+            //throw error;
         } 
         
         res.writeHead(200);
@@ -23,6 +23,18 @@ function createServer(req, res) {
     if(path == "/" || path == ""){
         path = "/index.html"
     }
-    doc = fs.readFile(__dirname + path, fsCallback);
+
+    var quest = path.indexOf("?");
+    var js = path.indexOf(".js");
+    if (quest != -1) {
+        path = path.slice(0,quest);
+    }
+
+    if (js == -1){
+        path = "/html_Pages" + path;
+        doc = fs.readFile(__dirname + path, fsCallback); //url used to be __dirname
+    } else {
+        fs.readFile(__dirname + path, fsCallback);
+    }
 }
 app.listen(8000);
