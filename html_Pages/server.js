4ccadc33b65e97a3b30f2d08803b7a2cd0f06ad3
Nick Turner <nickpturner98@protonmail.com>
2021-04-28 12:27:23 -0400

    made a function server.js file. index.html is bad

---
+++ html_Pages/server.js
@@ -0,0 +1,32 @@
+//TESTING A SERVER
+var http=require('http');
+var fs = require('fs'); 
+var url = require('url');
+var app = require('http').createServer(createServer);
+
+function createServer(req, res) {
+    var path = url.parse(req.url).pathname;
+    var fsCallback = function(error, data) {
+        if(error){
+            res.writeHead(404);
+            res.write(data);
+            res.end();
+            doc = fs.readFile(__dirname + "/404.html", fsCallback);
+            throw error;
+        } 
+        
+
+        res.writeHead(200);
+        res.write(data);
+        res.end();
+    }
+    //THING 1 from test.js was here
+   
+    if(path == "/" || path == ""){
+        path = "/index.html"
+    }
+    doc = fs.readFile(__dirname + path, fsCallback);
+}
+app.listen(8000);
+
+//THING 2 from test.js was here
--- pages/index.js
+++ pages/index.js
@@ -1,33 +1,4 @@
 import React from 'react';
-//import Button from '@material-ui/core/Button';
-//import { makeStyles } from '@material-ui/core/styles';
-//import '../pages/style.css'
-
-/*export default function Home(props) 
-{
-  	return(
-		<div className="index">
-  		<div className="logo">UMW</div>
-  		<div className="sublogo">CPSC Major Opprotunities</div>
-			<hr color="white" />
-			<br /><br />
-			<center><div className="desc">
-				New Here? 
-				<br /><br />
-				//This links to the "login.html" file
-				<button class="gradBtn" onclick='document.location.href = "./register.html";'>Create Account</button>
-			</div>
-			<div className="desc">
-				Already have an account? Welcome back!
-				<br />
-				<br />
-				<button class="gradBtn" onclick='document.location.href = "./login.html";'>Student Login</button> &nbsp;&nbsp;&nbsp;
-				<button class="gradBtn" onclick='document.location.href = "./login.html";'>Employee / Alumni Login</button> &nbsp;&nbsp;&nbsp;
-				<button class="gradBtn" onclick='document.location.href = "./login.html";'>Admin Login</button> 
-		</div></center>
-		</div>
-	);
-}*/
 
 var http = require('http'),
     fs = require('fs');
