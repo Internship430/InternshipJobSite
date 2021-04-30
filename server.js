9897c19d4915b6759d5b063ecc57bc888e1097e3
Nick Turner <nickpturner98@protonmail.com>
2021-04-29 21:29:27 -0400

    edited server.js but still shows code on site

--- server.js
+++ server.js
@@ -1,5 +1,5 @@
 //TESTING A SERVER
-var http=require('http');
+var http = require('http');
 var fs = require('fs'); 
 var url = require('url');
 var app = require('http').createServer(createServer);
@@ -7,29 +7,33 @@ var app = require('http').createServer(createServer);
 
 function createServer(req, res) {
     var path = url.parse(req.url).pathname;
-    var fsCallback = function(error, data) { 
-        if(error){ //catches errors
-            //doc = fs.readFile(__dirname + "/404.html", fsCallback);
-            throw error;
-        } 
-        
-        res.writeHead(200);
-        res.write(data);
-        res.end();
-    }
-   
-    if(path == "/" || path == ""){
+    if(path == "/" || path == ""){  //if there is no location specified go to index
         path = "/index.html"
     }
+    
+    var fsCallback = function(error, data) { 
+        if(path.includes("favicon.ico")){
+            res.writeHead(204).end;
+        } else if(error){ //catches errors
+            //doc = fs.readFile(__dirname + "/404.html", fsCallback);
+            throw error;
+        } else {
+        
+            res.writeHead(200); //returns connection is good
+            res.write(data);
+            res.end();
+        }
+    }
+
 
     var full_path = path; //in case we need the whole thing later
     var quest = path.indexOf("?");
-    var js = path.indexOf(".js");   //checks if path javascript file
+    var js = path.indexOf(".js");   
     if (quest != -1) {
         path = path.slice(0,quest); //removes everything after question mark
     }
 
-    if (js == -1){
+    if (js == -1){ //checks path a is not javascript file
         path = "/html_Pages" + path; //looking in html_Pages dir for html file
         doc = fs.readFile(__dirname + path, fsCallback);
     } else {
